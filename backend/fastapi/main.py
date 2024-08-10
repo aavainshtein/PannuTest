5150accbf2e2f675c012e9cb80d8a9eb8b47e6ca
Aleksandr Vainshtein <asderq@gmail.com>
2024-08-10 19:15:02 +0300

    fastapi: test graphql schema

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,15 +1,26 @@
-from typing import Union
-
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+import strawberry
 
+# Определяем типы и запросы для GraphQL
+@strawberry.type
+class Query:
+    hello: str = "Hello, World!"
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def process_text(self, text: str) -> str:
+        return text.upper()
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+# Создание FastAPI приложения и подключение GraphQL
 app = FastAPI()
 
+graphql_app = GraphQLRouter(schema)
+app.include_router(graphql_app, prefix="/graphql")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"message": "Go to /graphql for GraphQL API"}
