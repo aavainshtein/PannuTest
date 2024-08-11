085a0ba84884ea1726ee3206816a97fb56cbcee9
Aleksandr Vainshtein <asderq@gmail.com>
2024-08-11 17:30:03 +0300

    hasura: create table keyword added created_at

---
+++ backend/hasura/migrations/default/1723384565382_create_table_public_keyword/up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+    "public"."keyword" (
+        "id" uuid NOT NULL DEFAULT gen_random_uuid (),
+        "created_at" timestamptz NOT NULL DEFAULT now (),
+        "title" text NOT NULL,
+        PRIMARY KEY ("id"),
+        UNIQUE ("title")
+    );
+
+COMMENT ON TABLE "public"."keyword" IS 'keywords for posts';
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
