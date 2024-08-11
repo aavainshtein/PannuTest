7152fee7123b7a802cd85b3a61f261db4d8f0c9b
Aleksandr Vainshtein <asderq@gmail.com>
2024-08-11 16:51:51 +0300

    hasura: create table post

---
+++ backend/hasura/migrations/default/1723380886543_create_table_public_post/up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "public"."post" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "author_id" uuid NOT NULL, "title" text NOT NULL, "content" text NOT NULL, "is_public" boolean NOT NULL DEFAULT true, PRIMARY KEY ("id"), FOREIGN KEY ("author_id") REFERENCES "auth"."users"("id") ON UPDATE restrict ON DELETE restrict );COMMENT ON TABLE "public"."post" IS E'user\'s post';
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_post_updated_at"
+BEFORE UPDATE ON "public"."post"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_post_updated_at" ON "public"."post"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE  INDEX "idx_posts_author_id" on
+  "public"."post" using btree ("author_id");
+CREATE  INDEX "idx_posts_is_public" on
+  "public"."post" using btree ("is_public");
+CREATE  INDEX "idx_posts_created_at" on
+  "public"."post" using btree ("created_at");
+
